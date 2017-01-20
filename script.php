b6053cec05d067bc617455771aacbda57745b3d9
Felipe Peña <felipe@penya.cl>
2017-01-19 18:12:51 -0800

    Ejemplo completo

--- script.php
+++ script.php
@@ -1,9 +1,67 @@
 <?php
-define("ACTIVECAMPAIGN_URL", "API_URL");
-define("ACTIVECAMPAIGN_API_KEY", "API_KEY");
+
+define("ACTIVECAMPAIGN_URL", "https://cervecerakunstmann.api-us1.com");
+define("ACTIVECAMPAIGN_API_KEY", "[key]");
+
+/*
+Para regiones:
+"Metropolitana" => "Metropolitana de Santiago"
+"I Región" => "Tarapacá"
+"II Región" => "Antofagasta"
+"III Región" => "Atacama"
+"IV Región" => "Coquimbo"
+"V Región" => "Valparaíso"
+"VI Región" => "O'Higgins"
+"VII Región" => "Maule"
+"VIII Región" => "Bío Bío"
+"IX Región" => "La Araucanía"
+"X Región" => "Los Lagos"
+"XI Región" => "Aysén"
+"XII Región" => "Magallanes"
+"XIV Región" => "Los Ríos"
+"XV Región" => "Arica y Parinacota"
+"Fuera de Chile" => "Fuera de Chile"
+
+Para variedades:
+"TOROBAYO" => "Torobayo"
+"LAGER" => "Lager"
+"BOCK" => "Bock"
+"LAGER SIN FILTRAR" => "Lager Sin Filtrar"
+"ARÁNDANOS" => "Arándano"
+"MIEL" => "Miel"
+"TRIGO" => "Trigo"
+"SIN ALCOHOL" => "Lager Sin Alcohol"
+"SOMMER PILS" => "Sommer Pils"
+"CHOCOLATE" => "Chocolate"
+"ANWANDTER" => "Anwandter"
+"DOPPEL BOCK" => "Doppelbock"
+"GRAN TOROBAYO" => "Gran Torobayo"
+"TOROBAYO SIN FILTRAR" => "Torobayo Sin Filtrar"
+"SESSION IPA" => "Session Ipa"
+
+*/
+
 require_once("includes/ActiveCampaign.class.php");
 $ac = new ActiveCampaign(ACTIVECAMPAIGN_URL, ACTIVECAMPAIGN_API_KEY);
+$contact = array(
+	"email"	=> "felipe@penya.cl",
+	"name" => "Felipe Peña",
+	"p[{$list_id}]"      => 1,
+	"status[{$list_id}]" => 1, 
+	"field[3,0]" =>  "Los Ríos", // Valor región 
+	"field[4,0]" =>  "||Torobayo||Lager||" //Valores seleccionados por usuario, separados por || 
+);
 
+$contact_sync = $ac->api("contact/sync", $contact);
 
+if (!(int)$contact_sync->success) {
+	// request failed
+	echo "<p>Error al sincronizar usuario. Error: " . $contact_sync->error . "</p>";
+	exit();
+}
+        
+// successful request
+$contact_id = (int)$contact_sync->subscriber_id;
+echo "<p>Contacto actualizado y sincronizado exitosamente (ID {$contact_id})!</p>";
 
 ?>
